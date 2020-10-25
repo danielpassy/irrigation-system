8754f602b5563d7e40b7c118b395993356f126da
daniel passy <danielpassy@hotmail.com>
2020-10-25 12:04:30 -0300

    turned the main loop into an async func and created a class wrapper

--- src/NodeScheduleTest.js
+++ src/NodeScheduleTest.js
@@ -1,20 +1,10 @@
 var schedule = require('node-schedule');
 const devicehandler = require('./Irrigation')
-let jobList = []
 
-function jobFactory(date, instructions){
-    return schedule.scheduleJob(date, instructions)
-}
 
-function addJob(date, instructions){
-    if (jobList.length>=2){
-        jobList.pop()
-    }
-    jobList.push(jobFactory(date, instructions))
-}
-async function CancelWrapper(task, devices){
+async function CancelWrapper(task, devices) {
     console.log('turning on the inner job at ' + new Date());
-    async function returnFunction(){
+    async function returnFunction() {
         await task(devices)
         await jobList[1].cancel()
     }
@@ -22,17 +12,64 @@ async function CancelWrapper(task, devices){
 }
 
 
+class FlexibleScheduler {
 
-var turnOn = schedule.scheduleJob("32  22  *  *  *", function(fireDate){
+    constructor() {
+        let jobList = []
+        this.start()
 
-    console.log('turning on at ' + new Date());
-    devicehandler.setup().
-    then(([devices, credentials]) => devicehandler.getRequiredWateringTime(devices, credentials)).
-    then((devices, requiredWater) => {
-        addJob(requiredWater, function(){CancelWrapper(devicehandler.turnOff, devices)}
-    )})
+    }
 
-})
+    addJob(date, instructions) {
+        if (jobList.length >= 2) {
+            jobList.pop()
+        }
+        jobList.push(jobFactory(date, instructions))
+    }
 
-jobList.push(turnOn)    
+    jobFactory(date, instructions) {
+        return schedule.scheduleJob(date, instructions)
+    }
 
+    async CancelWrapper(task, devices) {
+        console.log('turning on the inner job at ' + new Date());
+        async function returnFunction() {
+            await task(devices)
+            await jobList[1].cancel()
+        }
+        return returnFunction()
+    }
+
+
+    start() {
+        schedule.scheduleJob("32  22  *  *  *", function (fireDate) {
+
+            console.log('turning on at ' + new Date());
+
+            devicehandler.setup().
+            then(([devices, credentials]) => devicehandler.getRequiredWateringTime(devices, credentials)).
+            then((devices, requiredWater) => {
+                    addJob(requiredWater, function () { CancelWrapper(devicehandler.turnOff, devices) }
+                    )
+            })
+        })
+        jobList.push(turnOn)
+    }
+
+
+}
+
+async function a (a){
+    return async function(a, b, c) {
+        let devices = await devicehandler.setup()
+        let requiredWater = await devicehandler.getRequiredWateringTime()
+        await addJob(requiredWater, function () {
+            CancelWrapper(devicehandler.turnOff, devices) 
+        })
+    }
+}
+
+module.exports = {
+    foo: foo,
+    bar: bar
+};
--- src/test.js
+++ src/test.js
@@ -1,10 +1,12 @@
-const devicehandler = require('./Irrigation')
-
-devicehandler.setup().
-then(([devices, credentials]) => devicehandler.getRequiredWateringTime(devices, credentials)).
-then((devices, requiredWater) => {addJob(requiredWater, turnOff(devices))})
+// const devicehandler = require('./Irrigation')
+const FlexibleScheduler = require('./NodeScheduleTest')
 
 
-// then((devices, credentials) 
-// it should be 2 arguments but it's returning one argument that's an array (inside devices)
-// check to see array descontruction
+// devicehandler.setup().
+// then(([devices, credentials]) => devicehandler.getRequiredWateringTime(devices, credentials)).
+// then((devices, requiredWater) => {addJob(requiredWater, turnOff(devices))})
+
+
+// // then((devices, credentials) 
+// // it should be 2 arguments but it's returning one argument that's an array (inside devices)
+// // check to see array descontruction
